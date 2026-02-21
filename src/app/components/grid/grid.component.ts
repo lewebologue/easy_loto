80a40c9075d2da06a2f04665ec11d220ee4dadb9
Ludovic <thuard.ludovic@gmail.com>
2026-02-21 11:15:20 +0100

    feat: ajouter l'affichage du dernier numéro et améliorer la gestion des numéros sélectionnés

--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LotoStateService } from '../../services/loto-state.service';
 
 @Component({
   selector: 'app-grid',
@@ -8,12 +9,15 @@ import { CommonModule } from '@angular/common';
   templateUrl: './grid.component.html',
   styleUrl: './grid.component.scss'
 })
-export class GridComponent {
+export class GridComponent implements OnInit {
   @Input() partyType: string = '';
   @Output() numbersChange = new EventEmitter<{ selectedNumbers: number[]; lastNumber: number | null }>();
 
   numbers: number[] = Array.from({ length: 90 }, (_, i) => i + 1);
   selectedNumbers: Set<number> = new Set();
+  lastNumber: number | null = null;
+
+  constructor(private lotoStateService: LotoStateService) {}
 
   toggleNumber(num: number): void {
     let lastNumber: number | null;
@@ -25,7 +29,10 @@ export class GridComponent {
       this.selectedNumbers.add(num);
       lastNumber = num;
     }
-    this.numbersChange.emit({ selectedNumbers: this.getSelectedNumbersSorted(), lastNumber });
+    const sorted = this.getSelectedNumbersSorted();
+    this.lastNumber = lastNumber;
+    this.numbersChange.emit({ selectedNumbers: sorted, lastNumber });
+    this.lotoStateService.updateNumbers(sorted, lastNumber);
   }
 
   isSelected(num: number): boolean {
@@ -36,8 +43,28 @@ export class GridComponent {
     return Array.from(this.selectedNumbers).sort((a, b) => a - b);
   }
 
+  getSelectedNumbersByDozen(): number[][] {
+    const dozens: number[][] = Array.from({ length: 9 }, () => []);
+    const sorted = this.getSelectedNumbersSorted();
+    
+    for (let num of sorted) {
+      const dozenIndex = Math.floor((num - 1) / 10);
+      dozens[dozenIndex].push(num);
+    }
+    
+    return dozens;
+  }
+
   resetGrid(): void {
     this.selectedNumbers.clear();
+    this.lastNumber = null;
     this.numbersChange.emit({ selectedNumbers: [], lastNumber: null });
+    this.lotoStateService.updateNumbers([], null);
+  }
+
+  ngOnInit(): void {
+    this.lotoStateService.state$.subscribe(state => {
+      this.lastNumber = state.lastNumber;
+    });
   }
 }
